aa152896ea37c4df1cfafb7637d9b950a5944955
Techie Dev <45366861+arshadkha0786@users.noreply.github.com>
2024-07-11 12:18:34 +0530

    Initial commit for Spring Boot Application (Employee Management)

---
+++ employeeManagement_springBoot_backend/src/main/java/com/bytbuddies/employeeManagement_springBoot_backend/EmployeeManagementSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bytbuddies.employeeManagement_springBoot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringBootBackendApplication.class, args);
+	}
+
+}
